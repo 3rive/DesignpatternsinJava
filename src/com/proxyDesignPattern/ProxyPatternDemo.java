042fe62d0871d194cf760b2fad6edaa9f542afd1
Hicckapps <3rive@gmail.com>
2016-08-29 13:16:09 +0800

    Change to debug
    Added some debug statements

--- src/com/proxyDesignPattern/ProxyPatternDemo.java
+++ src/com/proxyDesignPattern/ProxyPatternDemo.java
@@ -13,6 +13,7 @@ public class ProxyPatternDemo {
 	 * @param args
 	 */
 	public static void main(String[] args) {
+		System.out.println("Trying to load the image");
 		Image image = new ProxyImage("test.jpg");
 		image.display();
 		System.out.println(" ");
